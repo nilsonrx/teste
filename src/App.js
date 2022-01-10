6eb3b0c89112e5a3aa5111f345d44bd920645a1e
Nilson <nilsonrx@gmail.com>
2022-01-10 09:04:20 -0300

    Main

---
+++ src/App.js
@@ -0,0 +1,9 @@
+var React = require('react'); // importa a lib react
+var reactDOM = require('react-dom');// importa a lib react-dom
+var MeuComponente = require('./components/MeuComponente.js');
+
+reactDOM.render(document.getElementById('app'));
+
+
+
+
---
+++ src/components/MeuComponente.js
@@ -0,0 +1,11 @@
+var React = require('react'); // importa a lib react
+
+var MeuComponente = React.createClass({
+  render: function(){
+    return (
+    <h1>Hello World! </h1>
+    );
+  }
+});
+
+module.exports = MeuComponente;
